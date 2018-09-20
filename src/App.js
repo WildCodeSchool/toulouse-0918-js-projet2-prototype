e4bd6ff1855ac8c337cf8e83f833a99686d2bec5
Benoît Hubert <benoithubert@gmail.com>
2018-09-20 09:03:15 +0200

    Add hardcoded movie list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react';
 import MovieCard from './MovieCard';
 
+const movies = [
+  {
+    "title": "Pirates of the Caribbean: On Stranger Tides",
+    "img": "https://m.media-amazon.com/images/M/MV5BMjE5MjkwODI3Nl5BMl5BanBnXkFtZTcwNjcwMDk4NA@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+    "summary": "Jack Sparrow and Barbossa embark on a quest to find the elusive fountain of youth, only to discover that Blackbeard and his daughter are after it too."
+  },
+  {
+    "title": "Matrix",
+    "img": "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
+    "summary": "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers."
+  },
+  {
+    "title": "Interstellar",
+    "img": "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_SX675_AL_.jpg",
+    "summary": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival."
+  },
+  {
+    "title": "Hook",
+    "img": "https://m.media-amazon.com/images/M/MV5BNmJjNTQzMjctMmE2NS00ZmYxLWE1NjYtYmRmNjNiMzljOTc3XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+    "summary": "When Captain Hook kidnaps his children, an adult Peter Pan must return to Neverland and reclaim his youthful spirit in order to challenge his old enemy."
+  },
+  {
+    "title": "Aliens",
+    "img": "https://m.media-amazon.com/images/M/MV5BYzVlMWViZGEtYjEyYy00YWZmLThmZGEtYmM4MDZlN2Q5MmRmXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,664,1000_AL_.jpg",
+    "summary": "Ellen Ripley is rescued by a deep salvage team after being in hypersleep for 57 years. The moon that the Nostromo visited has been colonized, but contact is lost."
+  },
+  {
+    "title": "Star Wars: A New Hope",
+    "img": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg",
+    "summary": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station..."
+  }
+]
 class App extends Component {
   render() {
     return (
@@ -40,7 +72,7 @@ class App extends Component {
 
         <main role="main">
 
-          <section className="jumbotron text-center">
+          {/*<section className="jumbotron text-center">
             <div className="container">
               <h1 className="jumbotron-heading">Album example</h1>
               <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
@@ -49,15 +81,15 @@ class App extends Component {
                 <a href="#" className="btn btn-secondary my-2">Secondary action</a>
               </p>
             </div>
-          </section>
+          </section>*/}
 
           <div className="album py-5 bg-light">
             <div className="container">
 
               <div className="row">
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
+              {
+                movies.map((m, idx) => <MovieCard key={idx} movie={m} />)
+              }
               </div>
             </div>
           </div>
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const MovieCard = () => (
+const MovieCard = ({ movie: { title, img, summary } }) => (
   <div className="col-md-4">
     <div className="card mb-4 shadow-sm">
-      <img className="card-img-top" src="/img/thumbnail.svg" alt="Card image cap" />
+      <img className="card-img-top" src={ img || '/img/thumbnail.svg' } alt="Card image cap" />
       <div className="card-body">
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">{ title }</h5>
+        <p className="card-text">{ summary }</p>
         <div className="d-flex justify-content-between align-items-center">
           <div className="btn-group">
             <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
